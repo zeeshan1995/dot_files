8ecf094af2a8e51b84b7768a14eca688c7ee7238
Zeeshan Akhter <zakhter@microsoft.com>
2022-06-29 09:27:44 +0530

    Fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 sudo apt update
 sudo apt --yes upgrade
-sudo apt install gparted
-sudo apt install vim
-sudo apt install net-tools
-sudo apt install openssh-server
+sudo apt install gparted -y
+sudo apt install vim -y
+sudo apt install net-tools -y
+sudo apt install openssh-server -y
 
 #Install latest git
-sudo add-apt-repository ppa:git-core/ppa
+sudo apt purge git -y
+sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt update
-sudo apt install git
+sudo apt install git -y
+sudo apt-get install -y libwebkit2gtk-4.0-37
 
 
 # Install gcm
 # https://github.com/GitCredentialManager/git-credential-manager/releases
-sudo wget https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.696/gcmcore-linux_amd64.2.0.696.deb /tmp/gcmcore.deb
-sudo dpkg -i /tmp/gcmcore.deb
+sudo wget https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.696/gcmcore-linux_amd64.2.0.696.deb -P /tmp/
+sudo dpkg -i /tmp/gcmcore-linux_amd64.2.0.696.deb
 
 #Configure Git cache
 git config --global credential.helper manager-core
